582b300b8e3d19d15631db0c3bc2b68feb2b5401
Peter Ho <petho@vncam-barg8wp.local>
2017-03-02 15:35:36 -0800

    Understanding how Debounced function works.

---
+++ DebouncFunction/DebouncedSwitch.swift
@@ -0,0 +1,21 @@
+//
+//  DebouncedSwitch.swift
+//  DebouncFunction
+//
+//  Created by Peter Ho on 2017-03-02.
+//  Copyright © 2017 Peter Ho. All rights reserved.
+//
+
+import UIKit
+
+class DebouncedSwitch: UISwitch {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- DebouncFunction/ViewController.swift
+++ DebouncFunction/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var debouncedFunction: (()->())?
+    
+    func debounce(delay: Int, queue: DispatchQueue, action: @escaping (() -> ())) -> ()->() {
+        var lastFireTime = DispatchTime.now()
+        print("lastFireTime = \(lastFireTime.rawValue)")
+        let dispatchDelay = DispatchTimeInterval.milliseconds(delay)
+        
+        return {
+            let dispatchTime: DispatchTime = lastFireTime + dispatchDelay
+            queue.asyncAfter(deadline: dispatchTime, execute: {
+                let when: DispatchTime = lastFireTime + dispatchDelay
+                let now = DispatchTime.now()
+                if now.rawValue >= when.rawValue {
+                    lastFireTime = DispatchTime.now()
+                    action()
+                } else {
+                    print("now(\(now.rawValue)) < when(\(when.rawValue))")
+                }
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        debouncedFunction = debounce(delay: 5000, queue: DispatchQueue.main, action: {
+            print("repeat \(Date().timeIntervalSinceReferenceDate)")
+        })
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func start(_ sender: Any) {
+        DispatchQueue.global(qos: .background).async {
+            for index in 1...100000 {
+                print("Index - \(index)")
+                self.debouncedFunction?()
+            }
+        }
+    }
 
+    @IBAction func calculate(_ sender: Any) {
+        let lastFireTime = DispatchTime.now()
+        print("lastFireTime = \(lastFireTime.rawValue)")
+        let dispatchDelay = DispatchTimeInterval.milliseconds(1000)
+        let when = lastFireTime + dispatchDelay
+        let now = DispatchTime.now()
+        print("now = \(now.rawValue), when = \(when.rawValue)")
+    }
+    
+    @IBAction func startManually(_ sender: Any) {
+        debouncedFunction?()
+    }
 }
 
